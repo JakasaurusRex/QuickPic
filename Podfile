8c9114d5bfd1b78956b18ac7e5ba0016491274a5
Jake Torres <jakert2244@gmail.com>
2022-06-27 14:06:46 -0700

    Added initial stuff

---
+++ Podfile
@@ -0,0 +1,19 @@
+#r Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'QuickPic' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for QuickPic
+  pod 'Parse'
+  target 'QuickPicTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'QuickPicUITests' do
+    # Pods for testing
+  end
+
+end
--- QuickPic/AppDelegate.m
+++ QuickPic/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -16,6 +17,17 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    ParseClientConfiguration *config = [ParseClientConfiguration  configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+
+            configuration.applicationId = @"kSFttJdtxZ7EfzqncrEspW1boi3oWIiFOCaKQWbM"; // <- UPDATE
+            configuration.clientKey = @"LOVHWEtbR9UqRewrkxmO6dkno0ZQFpQkowjNEq6e"; // <- UPDATE
+            configuration.server = @"https://parseapi.back4app.com";
+    }];
+
+    [Parse initializeWithConfiguration:config];
+
+    
+    
     return YES;
 }
 
