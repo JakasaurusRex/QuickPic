d0e5310276c809ee2b343a5827640b4f9873882c
Jake Torres <jakert2244@gmail.com>
2022-06-27 18:10:53 -0700

    Created Post model class

--- QuickPic/ViewControllers/PostViewController.m
+++ QuickPic/ViewControllers/PostViewController.m
@@ -7,9 +7,9 @@
 
 #import "PostViewController.h"
 
-@interface PostViewController ()
+@interface PostViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *postImage;
-@property (weak, nonatomic) IBOutlet UITextView *captionField;
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
 
@@ -18,6 +18,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.textView.delegate = self;
+    [self pictureRecognizerGesture];
+}
+
+- (void) pictureRecognizerGesture {
+    UITapGestureRecognizer *gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapImage)];
+    [self.postImage addGestureRecognizer:gesture];
+    [self.postImage setUserInteractionEnabled:YES];
 }
 
 - (IBAction)cancelButton:(id)sender {
@@ -26,6 +34,41 @@
 }
 - (IBAction)shareButton:(id)sender {
     
+}
+
+-(void) didTapImage {
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+
+    // The Xcode simulator does not support taking pictures, so let's first check that the camera is indeed supported on the device before trying to present it.
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    // Do something with the images (based on your use case)
+    self.postImage.image = editedImage;
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void) textViewDidBeginEditing:(UITextView *) textView {
+  [textView setText:@""];
+  //other awesome stuff here...
 }
 
 /*
