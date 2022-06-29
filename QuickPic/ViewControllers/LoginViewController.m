e809ae6a75e0227363396cd8a6f32b1834ac09bb
Jake Torres <jakert2244@gmail.com>
2022-06-29 15:04:30 -0700

    User reset password capabilities

--- QuickPic/ViewControllers/LoginViewController.m
+++ QuickPic/ViewControllers/LoginViewController.m
@@ -12,6 +12,7 @@
 @interface LoginViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *password;
 @property (weak, nonatomic) IBOutlet UITextField *username;
+@property (strong, nonatomic) UIActivityIndicatorView *activityIndicator;
 
 @end
 
@@ -90,6 +91,59 @@
             [self performSegueWithIdentifier:@"loginSegue" sender:nil];
         }
     }];
+}
+- (IBAction)forgotPass:(id)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Reset password"
+                                                                               message:@"Please enter the email you added on your profile:"
+                                                                        preferredStyle:(UIAlertControllerStyleAlert)];
+    [alert addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = @"Email";
+    }];
+    
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
+                                                       style:UIAlertActionStyleDefault
+                                                     handler:^(UIAlertAction * _Nonnull action) {
+                                                             // handle response here.
+                                                     }];
+    [alert addAction:cancelAction];
+    
+    [alert addAction:[UIAlertAction actionWithTitle:@"Reset" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+        NSArray *textFields = alert.textFields;
+        UITextField *text = textFields[0];
+        [self resetPassword:text.text];
+        [self pause];
+    }]];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+- (void) resetPassword: (NSString *)email {
+    NSString *emailLower = [email lowercaseString];
+    emailLower = [emailLower stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+    [PFUser requestPasswordResetForEmailInBackground:emailLower block:^(BOOL succeeded, NSError * _Nullable error) {
+        if(error == nil) {
+            [self alertWithTitle:@"Success" message:@"Success! Check your email for further instructions. Please allow up to 5 minutes to receive your email."];
+        } else {
+            [self alertWithTitle:@"Error!" message:@"Could not complete request."];
+        }
+        [self unpause];
+    }];
+}
+
+-(void) pause {
+    self.activityIndicator = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+    self.activityIndicator.center = self.view.center;
+    self.activityIndicator.hidesWhenStopped = true;
+    [self.activityIndicator setActivityIndicatorViewStyle:UIActivityIndicatorViewStyleMedium];
+    [self.view addSubview:self.activityIndicator];
+    [self.activityIndicator startAnimating];
+    [self.view setUserInteractionEnabled:NO];
+}
+
+-(void) unpause{
+    [self.activityIndicator stopAnimating];
+    [self.view setUserInteractionEnabled:YES];
 }
 
 
